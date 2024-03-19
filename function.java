5037d0cbc6e70336bfa4b11e8c3ef2945cf8d354
siddharth-09 <1234siddhart.panchal@gmail.com>
2024-03-19 17:48:38 +0530

    done

--- function.java
+++ function.java
@@ -2,23 +2,39 @@ import java.util.Scanner;
 
 public class function {
 
-    public static int printhelloworld(){
-        System.out.println("Hello World");
-        return 0;
-    }
-    public static void Calculatesum(int num1 , int num2){//Parameters or Formal parameters
+    // public static int printhelloworld(){
+    //     System.out.println("Hello World");
+    //     return 0;
+    // }
+    // public static void Calculatesum(int num1 , int num2){//Parameters or Formal parameters
 
 
-            int sum = num1 + num2;
+    //         int sum = num1 + num2;
 
-            System.out.println("Your sum is = " + sum);
-    }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        sc.close();
-      Calculatesum(a , b);// aa je che ee Arguments che or actual parameters
+    //         System.out.println("Your sum is = " + sum);
+    // }
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     int a = sc.nextInt();
+    //     int b = sc.nextInt();
+    //     sc.close();
+    //   Calculatesum(a , b);// aa je che ee Arguments che or actual parameters
       
+    // }
+
+    // function overloading using data types 
+
+    // function to calculate int sum
+    public static int sum(int a , int b){
+        return a+b;
+    }
+
+    public static float sum(float a, float b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(3,5));
+        System.out.println(sum(3.2f,4.8f));
     }
 }
---
+++ functions/Prime_or_not.java
@@ -0,0 +1,64 @@
+package functions;
+
+import java.util.Scanner;
+
+public class Prime_or_not {
+    // galat hatu maru logic
+    // //function to check the number is prime or not
+    // public static int isPrime(int a){
+    // if(a%2 != 0 || a == 2){
+    // System.out.println(a +"Number is Prime");
+    // }
+    // else{
+    // System.out.println(a +" Number is not prime");
+    // }
+    // return 0;
+    // }
+
+    // // the main class
+    // public static void main(String args[]){
+    // System.out.println("Enter a number to check it is Prime or not");
+    // Scanner sc = new Scanner(System.in);
+    // int a = sc.nextInt();
+    // sc.close();
+    // System.out.println(isPrime(a));
+    // }
+
+    
+    // ave khabar padi su karavanu che chal jo
+
+    // function to calculate prime or not
+    // this is for n>=2
+    public static boolean isPrime(int n) {
+        // boolean isPrime = true; agar aa na lakhiye to bhi clase
+
+        // corner case
+        // for 2
+        if (n == 2) // checking for 2 only
+        {
+            return true;
+        } 
+        else if (n >= 2) // greater than 2
+        {
+            for (int i = 2; i <= n - 1; i++) {// loop to check which number will completly divide the 'n' number
+                if (n % i == 0) {// completly divide thay toh ee number prime nathi
+                    // isPrime = false;
+                    // break; no need for this we can directly write that return false when
+                    // condition is true
+                    return false;
+                }
+            }
+        }
+        else{// for 1 and 0 or other cases if there
+            return false;
+        }
+        // return isPrime; direct return true lakhi devanu
+        return true;
+    }
+
+    // creating a main class
+    public static void main(String[] args) {
+        System.out.println(isPrime(555));
+    }
+
+}
